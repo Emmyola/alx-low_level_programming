be23da8d5cc0265b934d7441f913d7b7d63b2359
Emmyola <oluwatobi275@gmail.com>
2022-09-20 15:38:01 -0700

    c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@
 * void prototypes()
 * int prototypes()
 */
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -18,3 +19,4 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
 
 #endif /*MAIN_H*/
+
