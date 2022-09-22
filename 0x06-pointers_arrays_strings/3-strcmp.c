96d0c641e3f6eeae68d55ea792cbde18477974d1
Emmyola <oluwatobi275@gmail.com>
2022-09-22 16:17:18 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,10 @@
  * @s2: param2
  * Return: integer
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2);
 {
-strcmp(s1, s2);
-return (dest);
+int result;
+result = strcmp(s1, s2);
+return (result);
 }
 
