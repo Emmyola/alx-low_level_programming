4c856823deaafdc9e8db31070852048e61ffedf4
Emmyola <oluwatobi275@gmail.com>
2022-09-22 14:31:29 -0700

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,15 @@
 char *_strcat(char *dest, const char *src)
 
 {
-   int len = 0,1;
+   int len = 0, 1;
    
    while (dest[len])
          len++;
-   for (i = 0; src[i] != '\0'; i++)
+
+   for (i = 0; src[i] != '0'; i++)
    {
       dest[len] = src[i];
-	len += 1;= 
+	len += 1;
    } 
     dest[len] = '\0';
     return (dest);
