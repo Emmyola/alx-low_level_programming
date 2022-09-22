c2fc41527f12206daeec5a0bc79c7e459daac3ef
Emmyola <oluwatobi275@gmail.com>
2022-09-22 16:10:19 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp ->  comparing purpose
+ * @s1: param1
+ * @s2: param2
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+strcmp(s1, s2);
+return (dest);
+}
+
