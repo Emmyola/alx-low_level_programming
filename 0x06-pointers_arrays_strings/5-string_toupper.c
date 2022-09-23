f082c1493e4711f613f10640d50074348892f727
Emmyola <oluwatobi275@gmail.com>
2022-09-22 17:28:43 -0700

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * String_toupper -> converting an string to array
+ * @X: string param
+ * Return: string
+ */
+char *string_toupper(char *)
+{
+int i = 0;
+while (x[i])
+{
+if(x[i] >= 97 && x[i] <= 122)
+x[i] = x[i] - 32;
+i++;
+}
+return(x);
+}
+
